25b8b9d77163e8f490dceb87d3292068db286942
Trilunaire <tristan.laurent96@gmail.com>
2016-01-30 12:20:41 +0100

    Ajout de la classe Menu Pause (pas encore implémentée dans les mini jeux), du support de freeTypeFont (pour le support et la conversion des polices) dans build.gradle. Ajout également de la police "PixelFont.TTF" dans les assets.

---
+++ android/assets/fonts/readme.txt
@@ -0,0 +1,27 @@
+...
+
+PUNKROCKTYPOGRAPHY.typeface
+
+Copyright: 1998-2000 william.clifford
+
+Website: http://www.williac.com
+
+
+License: You, the user, are permitted to use this typeface for your
+design/word processing purposes. You may use it for both private and
+commercial causes. You may redistribute this typeface only if you
+include this file (readme.txt) with it.
+
+This typeface does not have a complete character set.
+
+You use this typeface at your own risk.
+
+If you like this typeface, please tell me.
+
+If you would like to remix this typeface, please give me credit.
+
+I own this typeface. It is my property. I am letting you use it.
+
+My email address is punkrock@williac.com
+
+...
--- core/src/com/odysseedesmaths/menus/ScreenPause.java
+++ core/src/com/odysseedesmaths/menus/ScreenPause.java
@@ -1,5 +1,85 @@
 package com.odysseedesmaths.menus;
 
-public class ScreenPause extends Menu {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
+/**
+ * Created by trilunaire on 11/01/16.
+ */
+public class ScreenPause implements ApplicationListener{
+    BitmapFont font = null;
+    FreeTypeFontGenerator ftfg = null;
+    FreeTypeFontParameter ftfp = null;
+
+    Button retourJeu, recommencer,quitter;
+    TextButtonStyle txtButtonStyle=null;
+    Stage stage;
+
+    public void create(){
+        ftfp = new FreeTypeFontParameter();
+        font = new BitmapFont();
+        txtButtonStyle = new TextButton.TextButtonStyle();
+        ftfp.size= Gdx.graphics.getHeight()/ 10;
+        ftfp.color=new Color(255,255,255,1);
+        ftfg = new FreeTypeFontGenerator(Gdx.files.internal("fonts/pixel-life.TTF"));
+        font = ftfg.generateFont(ftfp);
+
+        txtButtonStyle.font = font;
+
+        retourJeu = new TextButton("Retour",txtButtonStyle);
+        recommencer = new TextButton("Recommencer",txtButtonStyle);
+        quitter = new TextButton("Quitter",txtButtonStyle);
+
+        Table tableau = new Table();
+        tableau.setFillParent(true);
+        //tableau.row().height(Gdx.graphics.getHeight() / 3);
+        tableau.add(retourJeu).width(Gdx.graphics.getWidth() / 3).pad(10);
+        tableau.getCell(retourJeu).expand();
+        tableau.row();
+        tableau.add(recommencer).width(Gdx.graphics.getWidth() / 3).pad(10);;
+        tableau.getCell(recommencer).expand();
+        tableau.row();
+        tableau.add(quitter).width(Gdx.graphics.getWidth() / 3).pad(10);;
+        tableau.getCell(quitter).expand();
+
+        stage = new Stage();
+
+        stage.addActor(tableau);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //batch.setProjectionMatrix(camera.combined)
+        stage.draw();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
